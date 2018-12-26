ba283d9ff91de5c3778e90649f1708e428fb8e3d
Neucrack <czd666666@gmail.com>
2018-12-26 17:38:52 +0800

    rebuild pages at 0103b49

---
+++ en/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd build
+
+git clone -b gh-pages https://github.com/Ai-Thinker-Open/GPRS_C_SDK_DOC.git ./old
+
+mv ./old/zh ./zh
+
+rm -rf ./old
+
+git init
+
+git config user.name $GIT_NAME
+
+git config user.email $GIT_EMAIL
+
+git remote add upstream "https://$GITHUB_TOKEN@github.com/Ai-Thinker-Open/GPRS_C_SDK_DOC.git"
+
+# comment below because we don't need history of gh-pages, and use git push --force to cover history
+# git fetch upstream
+
+# git reset upstream/gh-pages
+
+# echo "gprs.ai-thinker.com" > CNAME
+
+git add -A
+
+git commit -m "rebuild pages at ${rev}"
+
+git push upstream HEAD:gh-pages --force
+
